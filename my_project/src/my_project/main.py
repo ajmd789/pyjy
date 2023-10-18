754d180cbba39da1d8298da8058ca0e11a7c00ac
ajmd789 <fvffff@foxmail.com>
2023-10-18 23:28:28 +0800

    feat:能够获取股价
    下一步：优化获取股价频率和识别文本逻辑

--- my_project/src/my_project/main.py
+++ my_project/src/my_project/main.py
@@ -12,8 +12,21 @@ from utils import start_qq_from_the_taskbar
 from utils import start_soft_from_none
 from utils import showMaxJqka
 from utils import start_chrome
+from utils import get_difference_between_screen_shot
+from utils import current_time
 
 def main():
+    res = get_difference_between_screen_shot()
+    print(res,'<-识别',current_time())
+    if res:
+        send_stock_price_once(res)
+    # 等待1秒钟
+    else:
+        time.sleep(3)
+        # 再次调用main函数
+        main()
+
+def send_stock_price_once(code):
     # startQQ()
     # 从任务栏启动QQ
     start_qq_from_the_taskbar()
@@ -28,24 +41,19 @@ def main():
     if result is not None:
         print(f"鼠标成功移动到目标位置：{result}")
         doubleClick()
-        # 打开群聊天界面
-        copied_text = open_send_info()
-        if copied_text:
+        # # 打开群聊天界面
+        # copied_text = open_send_info()
+        if code:
             showDesktop()
-            start_chrome(copied_text)
-            
-            # "C:\Users\Tom\Pictures\aiPractice\jqkaIcon.jpg"
-            # can_start_jqka = start_soft_from_none(
-            #     r"C:\Users\Tom\Pictures\aiPractice\jqkaIcon.jpg")
-            # if can_start_jqka:
-            #     doubleClick()
-            #     showDesktop()
-            #     can_start_jqka = start_soft_from_none(
-            #     r"C:\Users\Tom\Pictures\aiPractice\jqkaIcon.jpg")
-            #     doubleClick()
-            #     showMaxJqka(copied_text)
-    else:
-        print("鼠标移动失败")
+            start_chrome(code)
+            print('本次股价发送完毕')
+    time.sleep(1)
+    while True:
+        res = get_difference_between_screen_shot()
+        print(res, '<-识别',current_time())
+        if res:
+            main()
+            break  # 执行完main()函数后退出循环
 
 
 if __name__ == "__main__":
